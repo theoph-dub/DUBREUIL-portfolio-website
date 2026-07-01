72109cfa78401a771279f63e806841fae8c561c5
theoph-dub <theophile.dubreuil32000@gmail.com>
2026-07-01 17:01:21 +0200

    multi-page vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/DUBREUIL-portfolio-website/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'index-read-more.html'),
+      },
+    },
+  },
 })
